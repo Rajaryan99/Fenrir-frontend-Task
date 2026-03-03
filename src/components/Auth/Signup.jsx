ac2acd14e69c4b45bbdb2655cab47a8a92dacdb4
Rajaryan singh <raj.aryan2518@gmail.com>
2026-03-03 20:58:57 +0530

    added narbar with logo

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
+import './Auth.css'
+
 
 export default function Signup() {
   return (
-    <div>
-      <h1>Signup Page</h1>
+    <div className="bg-[radial-gradient(circle_at_20%_30%,rgba(12,200,168,0.25),transparent_40%),radial-gradient(circle_at_80%_80%,rgba(255,119,0,0.35),transparent_40%),linear-gradient(135deg,#0f0f0f,#121212,#1a1a1a)] min-h-screen flex flex-col ">
+
+      <div className="logo p-6 ">
+        <img src={logo} alt="" className='h-[70px]' />
+      </div>
+
+    
+    
+   
+
     </div>
   )
 }
