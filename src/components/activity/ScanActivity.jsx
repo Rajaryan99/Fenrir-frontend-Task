c10788438f84c4a00679466cc46abb2175b71fd4
Rajaryan singh <raj.aryan2518@gmail.com>
2026-03-03 12:31:40 +0530

    initial setup for project

---
+++ src/components/activity/ScanActivity.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ScanActivity() {
+  return (
+    <div>
+      <h1>Scan Acticity page</h1>
+    </div>
+  )
+}
---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>DashBoard Page</h1>
+    </div>
+  )
+}
