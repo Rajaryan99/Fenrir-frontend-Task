9c808827765ad449b1dd83d12444a63f2e15ff8f
Rajaryan singh <raj.aryan2518@gmail.com>
2026-03-03 12:36:20 +0530

    created a routes using browserRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
+import Signup from './components/Auth/Signup'
+import Dashboard from './components/dashboard/Dashboard'
+import ScanActivity from './components/activity/ScanActivity'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+      <Routes>
+        <Route path='/signup' element={<Signup/> } />
+        <Route path='/Login' element={<Signup/> } />
+        <Route path='/' element={<Dashboard/> } />
+        <Route path='/activity' element={<ScanActivity/> } />
+      </Routes>
     
     </>
   )
