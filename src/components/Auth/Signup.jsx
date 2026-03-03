135313c43100c28204e4b4e90c4c6223affc0bfd
Rajaryan singh <raj.aryan2518@gmail.com>
2026-03-03 23:11:25 +0530

    sign up page

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -11,6 +11,18 @@ export default function Signup() {
         <img src={logo} alt="" className='h-[70px]' />
       </div>
 
+      <div className="leftsection text-white ">
+        <div className='heading text-6xl leading-20'>
+          <p>Expert level Cybersecurity in <span className='text-[#4daa9b]'>Houre</span> not weeks.</p>
+        </div>
+        <div className="details leading-8">
+          <h3 className='font-bold text-xl'>What's included</h3>
+          <p><i class="fa-solid fa-check "></i>Effortlessly spider and map targets to uncover hidden security flows</p>
+          <p><i class="fa-solid fa-check"></i>Deliver high-quality, validated findings in hours, not weeks.</p>
+          <p><i class="fa-solid fa-check"></i>Generated professional, enterprise-grade secuirty reports autoamtically.</p>
+        </div>
+      </div>
+
     
     
    
